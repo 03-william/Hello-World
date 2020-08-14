3b687ba99393da2df3507e2f74c951ddb3a06496
03-william <60370815+03-william@users.noreply.github.com>
2020-08-13 19:51:40 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hi, my name is William")
+		System.out.println("I am 17 years old")
+		System.out.println("I came from China")
+		System.out.println("I like to play sport")
+		System.out.println("I am a senior")
+	}
+}
